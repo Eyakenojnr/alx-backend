6f9b88eb675aace316a21c926b095f21c3d4d46a
Uduak Umanah <eyakenojnr@gmail.com>
2025-03-22 18:35:28 +0100

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python3
-"""
-Defines class Server that paginates a database of popular baby names
-"""
+
 import csv
 import math
 from typing import List, Tuple
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Takes 2 integer arguments and returns a tuple of size two
-    containing the start and end index corresponding to the range of
-    indexes to return in a list for those pagination parameters
-    Args:
-        page (int): page number to return (pages are 1-indexed)
-        page_size (int): number of items per page
-    Return:
-        tuple(start_index, end_index)
-    """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
+    """Return a tuple of start and end index for pagination"""
+    end_index = page * page_size
+    start_index = end_index - page_size
 
-    return (start, end)
-
+    return (start_index, end_index)
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -46,21 +31,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Takes 2 integer arguments and returns requested page from the dataset
-        Args:
-            page (int): required page number. must be a positive integer
-            page_size (int): number of records per page. must be a +ve integer
-        Return:
-            list of lists containing required data from the dataset
-        """
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        """Retrieve a specific page from the dataset"""
+        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
 
-        dataset = self.dataset()
-        data_length = len(dataset)
-        try:
-            index = index_range(page, page_size)
-            return dataset[index[0]:index[1]]
-        except IndexError:
-            return []
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        return data[start_index:end_index] if start_index < len(data) else []
