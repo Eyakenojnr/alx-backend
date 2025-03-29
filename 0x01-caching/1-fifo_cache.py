88e9f781228bdc95e4cbc1bb514cdb4361a6ca4e
Uduak Umanah <eyakenojnr@gmail.com>
2025-03-29 07:16:04 +0100

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,22 +8,25 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
+        self.order = []  # Track order of inserted keys
 
     def put(self, key, item):
         """Cache a key-item pair."""
         if key is None or item is None:
             return
+
+        if key not in self.cache_data:
+            self.order.append(key)
+
         self.cache_data[key] = item
 
         # If number of items in cache_data is higher than BaseCaching.MAX_ITEMS
         #   - Discard first item put in cache
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
+            first_key = self.order.pop(0)  # Remove first inserted key
             del self.cache_data[first_key]
             print(f"DISCARD: {first_key}")
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
-        if key is None or key not in self.cache_data:
-            return None 
-        return self.cache_data[key]
+        """Get an item by key."""
+        return self.cache_data.get(key, None)
