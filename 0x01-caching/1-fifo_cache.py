3d1b3bd55a637daaafef50bb4eb93e17e342a335
Uduak Umanah <eyakenojnr@gmail.com>
2025-03-29 04:11:38 +0100

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+"""Implements a FIFO cache."""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """Defines a FIFO caching system."""
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
         super().__init__()
-        self.order = []
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
+        """Cache a key-item pair."""
         if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
-            self.order.append(key)
-            self.cache_data[key] = item
+            return
+        self.cache_data[key] = item
+
+        # If number of items in cache_data is higher than BaseCaching.MAX_ITEMS
+        #   - Discard first item put in cache
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        """Return the value in self.cache_data linked to key."""
+        if key is None or key not in self.cache_data:
+            return None 
+        return self.cache_data[key]
