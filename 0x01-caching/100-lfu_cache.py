6c7bcd58b47a4f536ad1a4f1a3d63b2e28e55600
Uduak Umanah <eyakenojnr@gmail.com>
2025-04-02 22:14:05 +0100

    LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,66 +1,57 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+"""LFU Cache"""
+
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """Implements the LFU caching system."""
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
+        """Initialize."""
         super().__init__()
-        self.usage = []
-        self.frequency = {}
+        self.freq_count = {}  # Store usage frequency
+        self.lfu_order = OrderedDict()  # OrderedDict to maintain LRU order wihtin same frequency
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
+        """Assign key-value pair to cache_data and handle LFU eviction."""
         if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                lfu = min(self.frequency.values())
-                lfu_keys = []
-                for k, v in self.frequency.items():
-                    if v == lfu:
-                        lfu_keys.append(k)
-                if len(lfu_keys) > 1:
-                    lru_lfu = {}
-                    for k in lfu_keys:
-                        lru_lfu[k] = self.usage.index(k)
-                    discard = min(lru_lfu.values())
-                    discard = self.usage[discard]
-                else:
-                    discard = lfu_keys[0]
+            return
 
-                print("DISCARD: {}".format(discard))
-                del self.cache_data[discard]
-                del self.usage[self.usage.index(discard)]
-                del self.frequency[discard]
-            # update usage frequency
-            if key in self.frequency:
-                self.frequency[key] += 1
-            else:
-                self.frequency[key] = 1
-            if key in self.usage:
-                del self.usage[self.usage.index(key)]
-            self.usage.append(key)
+        if key in self.cache_data:
             self.cache_data[key] = item
+            self.freq_count[key] += 1  # Increase frequency count
+            self.lfu_order.move_to_end(key)  # Maintain LRU order
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Find the least frequently used keys
+                min_freq = min(self.freq_count.values())
+                lfu_keys = [k for k, v in self.freq_count.items() if v == min_freq]
+
+                # If multiple keys have same frequency, use LRU evict
+                if len(lfu_keys) > 1:
+                    for k in self.lfu_order.keys():
+                        if k in lfu_keys:
+                            discarded_key = k
+                            break
+                else:
+                    discarded_key = lfu_keys[0]
+
+                del self.cache_data[discarded_key]
+                del self.freq_count[discarded_key]
+                del self.lfu_order[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+            self.cache_data[key] = item
+            self.freq_count[key] = 1
+            self.lfu_order[key] = None   # Add to LRU order
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            self.frequency[key] += 1
-            return self.cache_data[key]
-        return None
+        """Retrieve item from cache_data and update its usage frequency"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.freq_count[key] += 1
+        self.lfu_order.move_to_end(key)  # Update LRU order
+        return self.cache_data[key]
