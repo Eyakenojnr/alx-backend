62985a325597556a3b3215f9ccf7aab5a717f54b
Uduak Umanah <eyakenojnr@gmail.com>
2025-04-02 03:01:52 +0100

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,44 +1,39 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+"""MRU cache"""
+
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """Implements the MRCU caching system."""
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
+        """Initialize class"""
         super().__init__()
-        self.usage = []
+        self.order = []
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
+        """Add an item to the cache."""
         if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[-1]))
-                del self.cache_data[self.usage[-1]]
-                del self.usage[-1]
-            if key in self.usage:
-                del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            self.cache_data[key] = item
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            mru_key = self.order.pop(-2)
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            return self.cache_data[key]
-        return None
+        """Retrieve an item by key."""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.order.remove(key)
+        self.order.append(key)
+
+        return self.cache_data[key]
